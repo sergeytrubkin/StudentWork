60bf332fbde298c2ff49c8d683b49851c60ee46b
Sergey Trubkin <trubkinsa@gmail.com>
2017-11-07 16:33:15 +0300

    Update 2017-11-07T13:33:06.104Z

--- js/custom.js
+++ js/custom.js
@@ -14,4 +14,45 @@ $(function() {
     $(".main-nav").toggleClass("main-nav--opened");
   });
 
+
+  /*  плавный скролл
+  ==================================  */
+
+  $(".main-nav__link, .logo__link").on("click", function(e){
+
+    e.preventDefault();
+    var currentBlock = $(this).attr("href"),
+    currentBlockOffset = $(currentBlock).offset().top;
+
+    $(".burger").removeClass("burger--close");
+    $(".main-nav").removeClass("main-nav--opened");
+
+    $("html, body").animate({
+      scrollTop: currentBlockOffset - 40
+    }, 750);
+
+  });
+
+
+  /*  фиксированное меню при скролле
+  ===================================*/
+
+  var headH = $("#js-main-menu").innerHeight(); // высота header'a
+
+  $(document).on("scroll", function() {
+
+    var documentScroll = $(this).scrollTop();
+
+    if(documentScroll > headH + 150) {
+      $("#js-main-menu").addClass("fixed main-nav__bg");
+      $("#js-header").css("paddingTop", headH);
+    } else {
+      $("#js-main-menu").removeClass("fixed main-nav__bg");
+      $("#js-header").removeAttr("style");
+    }
+  });
+
+  console.log(headH);
+
+
 });
